83449ce930a208feded16641a3652f41f4f8e49b
Vaibhav Maheshwai <vaibhav.dkm@gmail.com>
2016-10-29 20:03:20 +0530

    trial 1

--- app.py
+++ app.py
@@ -38,9 +38,9 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "got it, thanks!")
-
+                    if message_text == "Help":
+                        send_message(sender_id, "Welcome to edunova.io. We help everyone")
+                    send_message(sender_id, "This is how chatbots work sir?")    
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
 
