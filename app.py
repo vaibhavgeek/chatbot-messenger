cbe3784963c96dd3b3f0774c40df5fe850ca1597
Vaibhav Maheshwai <vaibhav.dkm@gmail.com>
2016-10-31 22:52:42 +0530

    adding remplate to chatbot

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import json
 
 import requests
 from flask import Flask, request
-
+from util import *
 import traceback
 import random
-
+from menu import *
 app = Flask(__name__)
 
 
@@ -25,72 +25,39 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-
+    try:
     # endpoint for processing incoming messaging events
-    payload = request.get_data()
-    sender, message = messaging_events(payload)
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
-    if message == "help":
-        send_message(sender , "You can choose topic you would like to learn and practice from the menu on left. For more information you can drop us a message and we will reply back to you shortly. ")
-    if data["object"] == "page":
+        payload = request.get_data()
+        sender, message = messaging_events(payload)
+        if message == "help":
+            help_menu()
+       
+        # This is for understnading and demo purposes. It gets json responses and shows on terminal    
+        data = request.get_json()
+        log(data)  # you may not want to log every incoming message in production, but it's good for testing
+        if data["object"] == "page":
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
+            for entry in data["entry"]:
+                for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+                    if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-                       
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
+                        if message_text.lower() == "help": 
+                            help_menu()
+                    if messaging_event.get("delivery"):  # delivery confirmation
+                        pass
 
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
+                    if messaging_event.get("optin"):  # optin confirmation
+                        pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+                    if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                        pass
 
-    return "ok", 200
+        return "ok", 200
 
-
-def send_message(recipient_id, message_text):
-
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-
-def messaging_events(payload):
-    """Generate tuples of (sender_id, message_text) from the
-    provided payload.
-    """
-    data = json.loads(payload)
-    messaging_events = data["entry"][0]["messaging"]
-    for event in messaging_events:
-        if "message" in event and "text" in event["message"]:
-            return (event["sender"]["id"], event["message"]
-                ["text"].encode('unicode_escape'))
-        elif "postback" in event and "payload" in event["postback"]:
-            return (event["sender"]["id"], event["postback"]["payload"])
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
---
+++ util.py
@@ -0,0 +1,155 @@
+import json
+import requests
+from constants import SEND_MESSAGE_URL, PAT as token
+from random import randint
+import random
+import xml.etree.ElementTree
+import math
+
+def send_typing_status(recipient):
+  """Send the message text to recipient with id recipient.
+  """
+  r = requests.post(SEND_MESSAGE_URL,
+    params={"access_token": token},
+      data=json.dumps({
+          "recipient": {"id": recipient},
+      "sender_action": "typing_on"
+    }),
+      headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+
+def get_solution_from_wolfarmAlpha(question):
+    url = "http://api.wolframalpha.com/v2/query"
+    params = {"input" : question, "appid" : "Q7K5HX-2Y24EKLAQW", "format" : 'image,plaintext'}
+    r = requests.get(url, params = params)
+    root = xml.etree.ElementTree.fromstring(r.text)
+    response = []
+    count = 0
+    for f in root:
+        if count == 0:
+            count = count + 1
+            continue
+        temp = {}
+        temp["title"] = f.attrib['title']
+        temp["img"] = f[0][0].attrib['src']
+        response.append(temp)
+    return response
+
+def send_text_message(recipient, text):
+  """Send the message text to recipient with id recipient.
+  """
+
+  r = requests.post(SEND_MESSAGE_URL,
+    params={"access_token": token},
+      data=json.dumps({
+          "recipient": {"id": recipient},
+      "message": {"text": text.decode('unicode_escape')},
+    }),
+      headers={'Content-type': 'application/json'})
+  if r.status_code != requests.codes.ok:
+    print r.text
+
+def send_button_template_message(recipient, text, buttons):
+    r = requests.post(SEND_MESSAGE_URL,
+          params={'access_token': token},
+          data=json.dumps({
+              "recipient": {"id": recipient},
+              "message": {
+                  "attachment": {
+                  "type": "template",
+                  "payload": {
+                      "template_type": "button",
+                      "text": text,
+                      "buttons": buttons
+                  }
+              }}
+          }),
+          headers={'Content-type': 'application/json'})
+    print r.text
+
+
+def send_image(recipent, item, type="image"):
+    r = requests.post(SEND_MESSAGE_URL, params = {'access_token' : token},
+            data = json.dumps({
+                "recipient" : {"id" : recipent},
+                "message" : {
+                    "attachment" : {
+                        "type" : type,
+                        "payload" : {
+                            "url" : item
+                        }
+                    }
+                }
+            }),
+            headers = {'Content-type' : 'application/json'}
+        )
+    print r.text
+
+def send_carasol_items(recipient, items):
+    r = requests.post(SEND_MESSAGE_URL,
+          params={'access_token': token},
+          data=json.dumps({
+              "recipient": {"id": recipient},
+              "message": {"attachment":
+                    {
+                        "type": "template",
+                        "payload": {
+                          "template_type": "generic",
+                          "elements": items
+                      }
+                 }
+            }
+        }),
+        headers={'Content-type': 'application/json'})
+    print r.text
+
+
+def generate_carasol_items(text, image_url, payload = None, showbtns = True):
+    if showbtns:
+        return {
+            "title": text,
+            "image_url": image_url,
+            "buttons": [
+                {
+                    "type": "postback",
+                    "title": "Learn This",
+                    "payload": payload
+                }
+            ]
+        }
+    else:
+        return {
+            "title": text,
+            "image_url": image_url,
+        }
+
+
+
+def generate_button(text, payload=None, type="text", url=None):
+    if type == "url":
+        return {
+            "type": "web_url",
+            "url": url,
+            "title": text
+        }
+    else:
+        return {
+            "type": "postback",
+            "title": text,
+            "payload": payload
+        }
+
+
+def messaging_events(payload):
+    """Generate tuples of (sender_id, message_text) from the
+    provided payload.
+    """
+    data = json.loads(payload)
+    messaging_events = data["entry"][0]["messaging"]
+    for event in messaging_events:
+        if "message" in event and "text" in event["message"]:
+            return (event["sender"]["id"], event["message"]
+                ["text"].encode('unicode_escape'))
+        elif "postback" in event and "payload" in event["postback"]:
+            return (event["sender"]["id"], event["postback"]["payload"])
