1eacd1efa35def35fbfa123e426121bfcef84590
Vaibhav Maheshwai <vaibhav.dkm@gmail.com>
2016-11-02 19:46:05 +0530

    debuug

--- app.py
+++ app.py
@@ -27,8 +27,6 @@ def webhook():
     payload = request.get_data()
     data = request.get_json()
     print data
-    message_t = get_message(data)
-    print message_t
     sender, message = messaging_events(payload)
     if message.lower() == "help":
         send_text_message(sender , "You can choose topic you would like to learn and practice from the menu on left. For more information you can drop us a message and we will reply back to you shortly. ")
